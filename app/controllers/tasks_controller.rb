bb6e2d03d3e7b74391b49462f6164af8b3376a3d
yy-kokubun <yy-kokubun@example.com>
2024-04-03 17:24:26 +0900

    【追加】Tasks MVC

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,49 @@
+class TasksController < ApplicationController
+    before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
+    def index
+      @tasks = Task.all
+    end
+  
+    def show
+    end
+  
+    def new
+      @task = Task.new
+    end
+  
+    def edit
+    end
+  
+    def create
+      @task = Task.new(task_params)
+      @task.save!
+      redirect_to tasks_path
+    rescue => e
+      flash[:alert] = e.message
+      render :new
+    end
+  
+    def update
+      @task.update!(task_params)
+      redirect_to tasks_path
+    rescue => e
+      render :edit
+    end
+  
+    def destroy
+      @task.destroy
+      redirect_to tasks_url
+    end
+  
+    private
+  
+    def set_task
+      @task = Task.find(params[:id])
+    end
+  
+    def task_params
+      params.require(:task).permit(:title, :points)
+    end
+  end
+  
