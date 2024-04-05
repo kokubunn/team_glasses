4e83e742f10ce35c69f1f0b428f66346b394d6e5
s-sato <s-sato@respect-pal.jp>
2024-04-05 17:58:02 +0900

    【修正】エラー解消時の差分

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_04_082348) do
   end
 
   create_table "tasks", force: :cascade do |t|
-    t.string "content"
+    t.string "title"
+    t.integer "points"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
